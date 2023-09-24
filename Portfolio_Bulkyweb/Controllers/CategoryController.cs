6ee1b25c54731303d059f469f89b060662998db9
Ahmed Qureshi <=>
2023-09-25 01:41:51 +0500

    SeedData Added To category

---
+++ Portfolio_Bulkyweb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Bulkyweb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
