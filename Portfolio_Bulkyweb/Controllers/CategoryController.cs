d64b35d6f64669850e52ee9e9a8b8c8770f4f461
Ahmed Qureshi <=>
2023-09-27 01:21:09 +0500

    Edit View is Under Construction

--- Portfolio_Bulkyweb/Controllers/CategoryController.cs
+++ Portfolio_Bulkyweb/Controllers/CategoryController.cs
@@ -42,5 +42,36 @@ namespace Portfolio_Bulkyweb.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null||id==0) 
+            {
+                return NotFound();
+            }
+           Category categoryFromDb =  _db.Categories.FirstOrDefault(c => c.Id == id);
+            //_db.Categories.Find(id);
+
+            if(categoryFromDb  == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
     }
 }
