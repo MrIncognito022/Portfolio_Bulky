602d18ac876def5c889195fd5080d34190c9abe5
Ahmed Qreshi <mr.incognito022@gmail.com>
2023-09-28 11:06:25 +0500

    Repository Interface is implemented

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //Let say T = Category
+
+        IEnumerable<T> GetAll();
+        //Get Indiviual Element where Expression respresents a LinQ Expression like (u => u.id = Id)
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,62 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            
+            this.dbSet = _db.Set<T>();
+            //This simply mean _db.Categories = dbSet
+            
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+            //Logic is just like 
+            //_db.Categories.where(u=>u.id = Id).FirstOrDefault();
+        }
+
+        IEnumerable<T> IRepository<T>.GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+            //Logic is just like 
+            //_db.Categories.ToList();
+        }
+
+        void IRepository<T>.Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        void IRepository<T>.RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+
+        void IRepository<T>.Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
