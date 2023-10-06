0a570a846a0ae61f08bcf76d953a14c97e19e04f
Ahmed Qreshi <mr.incognito022@gmail.com>
2023-10-06 13:43:58 +0500

    Home and Detail Page are Added to the Project

--- Portfolio_Bulkyweb/Areas/Customer/Controllers/HomeController.cs
+++ Portfolio_Bulkyweb/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Bulky.Models;
 using System.Diagnostics;
+using Bulky.DataAccess.Repository.IRepository;
 
 namespace Portfolio_Bulkyweb.Areas.Customer.Controllers
 {
@@ -8,17 +9,27 @@ namespace Portfolio_Bulkyweb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products;
+            products = _unitOfWork.Product.GetAll(includeProperties:"Category");
+            return View(products);
         }
 
+        public IActionResult Details(int id) 
+        {
+            Product product;
+            product = _unitOfWork.Product.Get(u=>u.Id == id,includeProperties:"Category");
+            return View(product);
+        }
         public IActionResult Privacy()
         {
             return View();
